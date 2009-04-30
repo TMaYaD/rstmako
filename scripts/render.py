2315c2e42ccdf21806b7f26d31a198266ee2e039
Subhash Chandra <TMaYaD@loonyb.in>
2009-04-30 16:32:50 +0530

    script entirely runs from python\n though it looses its change detection

--- scripts/render.py
+++ scripts/render.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python
 
-import sys ,os
+import sys
+from glob import glob
 
 from mako.template import Template
 from mako.lookup import TemplateLookup
+from mako.runtime import Context
 
 template_dir = 'templates/'
 public_dir = 'public/'
 
 lookup = TemplateLookup(directories=[template_dir], module_directory='/tmp/mako_modules')
 
-def main(argv):
-    for arg in argv:
-        print lookup.get_template(arg).render()
+def main():
+    templates = glob(template_dir + "*.mako")
+    for template in templates:
+    	ofile = template.replace(template_dir,public_dir,1).replace('mako','html')
+	buf = open(ofile,"w")
+	ctx = Context(buf)
+        print Template(filename=template, lookup=lookup).render_context(ctx)
 
-print os.getcwd()
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
