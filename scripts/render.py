4089c5a911f6b8f73df5394323ca3dfe5308a8ca
Subhash Chandra <TMaYaD@loonyb.in>
2009-04-30 12:57:30 +0530

    testing pre-commit hooks

---
+++ scripts/render.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+mylookup = TemplateLookup(directories=['../templates'], module_directory='/tmp/mako_modules')
+
+def serve_template(templatename, **kwargs):
+    mytemplate = mylookup.get_template(templatename)
+    print mytemplate.render(**kwargs)
+
+
+
