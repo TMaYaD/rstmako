f6cf9a9aae77e89c715f138eb61ee9ab24be7587
Subhash Chandra <TMaYaD@loonyb.in>
2009-04-30 16:13:02 +0530

    sample rst filter that simply adds tags

--- scripts/render.py
+++ scripts/render.py
@@ -8,10 +8,12 @@ from mako.lookup import TemplateLookup
 template_dir = 'templates/'
 public_dir = 'public/'
 
+rst2html="""
 def rst2html(rst):
     return "<rst>" + rst + "</rst>"
+"""
 
-lookup = TemplateLookup(directories=[template_dir], default_filters=['rst2html'])
+lookup = TemplateLookup(directories=[template_dir], default_filters=['rst2html'], imports=[rst2html])
 
 def main(argv):
     for arg in argv:
--- templates/test.mako
+++ templates/test.mako
@@ -1,2 +1,5 @@
+<% testvar="somenting unique" %>
+
 example mako template
 I expect tags around this whole thing
+and ${testvar} substitution
